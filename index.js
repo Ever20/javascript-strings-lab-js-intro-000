e24fbf71f8dc56b7cfa80a375b11fadb69badaf2
Francisco trejo <f85.trejo@gmail.com>
2021-02-17 19:14:09 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ var greeting = "Hello, everybody!";
 
 var specialGuest = "Neil deGrasse Tyson";
 
-var "Hello," = ""+ Neil deGrasse + "!";
+var +"Hello," = ""+ Neil deGrasse + "!";
 
 var topic = "space";
 
